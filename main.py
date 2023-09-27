00d72a2fe44dfddd605c1becbfad9e50365935a7
BramTeurlings <bram@dimenco.eu>
2023-09-27 17:01:30 +0200

    Started work on CR

--- main.py
+++ main.py
@@ -88,6 +88,7 @@ def extract_class_features(file_name, output_file_name, source_book_name):
     # Create a DataFrame
     data_dict = {
         'Class': class_names,
+        'Sub Feature Of': '',
         'Class Feature': feature_keys,
         'Description': descriptions,
         'Level': levels,
@@ -97,6 +98,150 @@ def extract_class_features(file_name, output_file_name, source_book_name):
 
     # Save the DataFrame to an Excel file
     df.to_excel(output_file_name)
+
+def extract_class_features_cr(file_name, output_file_name, source_book_name):
+    class_features = {}
+    class_name = None
+    class_feature_name = None
+    sub_feature_name = None
+    class_feature_description = None
+    class_feature_level = None
+
+    is_valid_class_feature = 1
+
+    # Regular expressions to match relevant lines
+    block_pattern = r'(?i)###Block: (.+) Class features'
+
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            is_valid_class_feature = 1
+            line = line.strip()
+
+            class_header_match = re.match(block_pattern, line)
+            if class_header_match:
+                class_name = class_header_match.group(1)
+                continue
+
+            # Split the line based on tab character or line end
+            parts = line.split("\t")
+            for part in parts:
+                if is_valid_class_feature:
+                    if part.startswith("KEY:"):
+                        feature_key = part.split("KEY:")[1]
+                        class_name_key = re.search(r'(.+)~', feature_key)
+                        if class_name_key and len(class_name_key.groups()) > 0:
+                            class_name = class_name_key.group(1).strip()
+                        class_feature_key = re.search(r'~(.+)', feature_key)
+                        if class_feature_key and len(class_feature_key.groups()) > 0:
+                            class_feature_name = class_feature_key.group(1).strip()
+                    elif part.startswith("CATEGORY:"):
+                        if not part.split("CATEGORY:")[1] == "Special Ability":
+                            is_valid_class_feature = 0
+                    elif part.startswith("TYPE:"):
+                        class_feature_type = part.split("TYPE:")[1]
+                        # TYPE:{Classname}ClassFeatures.(SpecialQuality/SpecialAttack)
+                        if re.search(r'(?i)SpecialQuality\.SuperNatural\.DomainPower', class_feature_type) or re.search(r'(?i)SpecialAttack\.SpellLike\.DomainPower', class_feature_type):
+                            # Dealing with cleric domain power, set sub_feature_name to class name
+                            sub_feature_name = class_name
+                            class_name = "Cleric"
+                        elif re.search(r'(?i)(.+)ClassFeatures\.SpecialAttack.Supernatural.BardicPerformance(.+)', class_feature_type) or re.search(r'(?i)(.+)ClassFeatures\.SpecialQuality.Supernatural.BardicPerformance(.+)', class_feature_type) or re.search(r'(?i)(.+)ClassFeatures\.SpecialAttack.Extraordinary.VersatilePerformance(.+)', class_feature_type):
+                            # Dealing with bardic/versatile performance, set sub_feature_name to class name
+                            sub_feature_name = class_name
+                            class_name = "Bard"
+                        elif re.search(r'(?i)(.+)ClassFeatures\.SpecialAttack(.+)', class_feature_type) or re.search(r'(?i)(.+)ClassFeatures\.SpecialQuality(.+)', class_feature_type):
+                            # Capture the parent class name between "ClassFeatures." and " ClassFeatures"
+                            type_match = re.search(r'(?i)(.+)ClassFeatures\.Special', class_feature_type)
+                            if type_match:
+                                if class_name and class_name != type_match.group(1):
+                                    # Class name was already set and differs from picked up key. Set key as sub_feature_name
+                                    sub_feature_name = class_name
+                                class_name = type_match.group(1)
+                        elif re.search(r'(?i)Class Feature\.Sorcerer Class Feature\.(.+) ~ Power LVL (.+).Sorcerer', class_feature_type):
+                            # Dealing with wizard school, set sub_feature_name to archetype name
+                            sub_feature_name = class_name
+                            class_name = "Sorcerer"
+                            # Capture the bloodline level
+                            bloodline_ability_level_match = re.search(r'~ Power LVL (.+).Sorcerer', class_feature_type)
+                            if bloodline_ability_level_match:
+                                class_feature_level = bloodline_ability_level_match.group(1)
+                        elif re.search(r'(?i)WizardClassFeatures\.SpecialQuality\.SuperNatural', class_feature_type) or re.search(r'(?i)WizardClassFeatures\.SpecialQuality\.SpellLike', class_feature_type):
+                            # Dealing with sorcerer bloodline, set sub_feature_name to archetype name
+                            sub_feature_name = class_name
+                            class_name = "Wizard"
+                        else:
+                            is_valid_class_feature = 0
+                    elif part.startswith("DESC:"):
+                        description_match = part.split("DESC:")[1]
+                        if description_match:
+                            if not class_feature_description:
+                                class_feature_description = ""
+                            class_feature_description += description_match
+                            if not class_feature_level:
+                                # Extract the level from the description if present
+                                for i in range(0, 4):
+                                    if i == 0:
+                                        level_match = re.search(r'(?i)At (\d+)s?t? level', class_feature_description)
+                                        if level_match:
+                                            break
+                                    if i == 1:
+                                        level_match = re.search(r'(?i)At (\d+)n?d? level', class_feature_description)
+                                        if level_match:
+                                            break
+                                    if i == 2:
+                                        level_match = re.search(r'(?i)At (\d+)r?d? level', class_feature_description)
+                                        if level_match:
+                                            break
+                                    if i == 3:
+                                        level_match = re.search(r'(?i)At (\d+)t?h? level', class_feature_description)
+                                        if level_match:
+                                            break
+                                if level_match:
+                                    class_feature_level = level_match.group(1)
+                                else:
+                                    class_feature_level = 0
+            if is_valid_class_feature and class_feature_name and class_feature_description:
+                class_feature_info = {
+                    'Class': class_name,
+                    'Name': class_feature_name,
+                    'SubName': sub_feature_name,
+                    'Description': class_feature_description if class_feature_description else '',
+                    'Level': class_feature_level if class_feature_level else 0
+                }
+
+                # Making sure the array doesn't get an exception by filling it with keys if they are not present.
+                if class_name not in class_features:
+                    class_features[class_name] = {}
+                if class_feature_name not in class_features[class_name]:
+                    class_features[class_name][class_feature_name] = []
+
+                # Appending the actual data.
+                class_features[class_name][class_feature_name].append(class_feature_info)
+
+            class_name = None
+            class_feature_name = None
+            class_feature_description = None
+            class_feature_level = None
+            sub_feature_name = None
+
+        # Convert the archetypes dictionary to a pandas DataFrame
+        archetype_data = []
+        for parent_class, archetypes_dict in class_features.items():
+            for archetype, features in archetypes_dict.items():
+                for feature in features:
+                    archetype_data.append({
+                        'Class': parent_class,
+                        'Sub Feature Of': feature['SubName'],
+                        'Class Feature': feature['Name'],
+                        'Description': feature['Description'],
+                        'Level': feature['Level'],
+                        'Source': source_book_name
+                    })
+
+        df = pd.DataFrame(archetype_data)
+
+        # Write the DataFrame to an Excel file
+        df.to_excel(output_file_name)
 
 def extract_archetype_info(file_path, output_file_name, source_book_name):
     archetypes = {}
@@ -366,14 +511,15 @@ if __name__ == '__main__':
     # Adds sub class feature from key in case of wizard schools: KEY:Air School ~ Air Supremacy
     # Adds cleric domain abilities to the same "Sub Class Feature" field
     # Adds sorcerer bloodline abilities to the same "Sub Class Feature" field. Bloodline ability template: TYPE:Class Feature.Sorcerer Class Feature.{BloodlineName} ~ Power LVL {level}.Sorcerer
-    file_path = 'C:\\Users\\Bram\\PycharmProjects\\ClassFeatureExtractor\\raw.githubusercontent.com_PCGen_pcgen_master_data_pathfinder_paizo_roleplaying_game_advanced_players_guide_apg_abilities_class.lst'
-    extract_archetype_info_apg(file_path, "apg_archetype_features.xlsx", "Pathfinder Roleplaying Game: Advanced Player's Guide")
+    # file_path = 'C:\\Users\\Bram\\PycharmProjects\\ClassFeatureExtractor\\raw.githubusercontent.com_PCGen_pcgen_master_data_pathfinder_paizo_roleplaying_game_advanced_players_guide_apg_abilities_class.lst'
+    # extract_archetype_info_apg(file_path, "apg_archetype_features.xlsx", "Pathfinder Roleplaying Game: Advanced Player's Guide")
 
     # Code specific to the CR:
-    # Todo: The CR uses a different key for class features, it looks like this: TYPE:{Classname}ClassFeatures.SpecialQuality. The code will have to be changed accordingly.
-    # file_path = 'C:\\Users\\Bram\\PycharmProjects\\ClassFeatureExtractor\\raw.githubusercontent.com_PCGen_pcgen_master_data_pathfinder_paizo_roleplaying_game_core_rulebook_cr_abilities_class.lst'
-    # extract_class_features(file_path, "cr_class_features.xlsx", "Pathfinder Roleplaying Game: Core Rulebook")
-    # extract_archetype_info(file_path, "cr_archetype_features.xlsx", "Pathfinder Roleplaying Game: Core Rulebook")
+    # Todo: The CR uses a different key for class features, it looks like this: CATEGORY:Special Ability TYPE:{Classname}ClassFeatures.(SpecialQuality/SpecialAttack). The code will have to be changed accordingly.
+    # Todo: Sorcerer bloodlines have to be handled the same way as the APG
+    # Todo: Cleric domains have to be handled the same way as the APG
+    file_path = 'C:\\Users\\Bram\\PycharmProjects\\ClassFeatureExtractor\\raw.githubusercontent.com_PCGen_pcgen_master_data_pathfinder_paizo_roleplaying_game_core_rulebook_cr_abilities_class.lst'
+    extract_class_features_cr(file_path, "cr_class_features.xlsx", "Pathfinder Roleplaying Game: Core Rulebook")
 
     # Code specific to the Ultimate Combat Guide:
     # Todo: The CR uses a different key for class features, it looks like this: TYPE:{Classname}ClassFeatures.SpecialQuality. The code will have to be changed accordingly.
